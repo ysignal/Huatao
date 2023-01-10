37d064dd683d8f992ffc31ab64c0a4f1777e47ec
minse <ysignal@163.com>
2023-01-10 14:13:21 +0800

    Initial Commit

---
+++ Huatao/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Huatao
+//
+//  Created by minse on 2023/1/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
